369d1bd966c26c3d3f2d84256199563fc5e721e0
hanhnv4 <hanhdevit@gmail.com>
2023-05-08 16:51:33 +0700

    [Fixbug] Remove file

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,48 @@
-import express from "express";
-import resizeImage from "./resize";
+import express from "express"
+import resizeImage from "./resize"
 
-const app = express();
-const port: number = 3000;
+const app = express()
+const port = 3000
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my project image");
-});
+  res.send("Welcome to my project image")
+})
 
 app.get("/api/images", async (req, res) => {
   try {
-    let filename: string = "";
-    let width: number = 200;
-    let height: number = 200;
+    let filename = ""
+    let width = 200
+    let height= 200
 
     if (req.query.width) {
-      width = Number(req.query.width);
+      width = Number(req.query.width)
     } else {
-      width = 200;
+      width = 200
     }
 
     if (req.query.height) {
-      height = Number(req.query.height);
+      height = Number(req.query.height)
     } else {
-      width = 200;
+      width = 200
     }
 
     if (req.query.filename) {
-      filename = req.query.filename.toString();
+      filename = req.query.filename.toString()
       const result = await resizeImage(filename, width, height)
       if (result) {
-        res.sendFile(
-          `${__dirname}/assets/thumb/${filename}.jpg`
-        );
+        res.sendFile(`${__dirname}/assets/thumb/${filename}.jpg`)
       }
     } else {
-      res.send('No file name no provide, Please check again!')
+      res.send("No file name no provide, Please check again!")
     }
   } catch (error) {
     console.log(error)
-     res.send('File name can be wrong, Plese check again!')
+    res.send("File name can be wrong, Plese check again!")
   }
-});
+})
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+  console.log(`App listening on port ${port}`)
+})
 
 export default app
-
--- src/resize.ts
+++ src/resize.ts
@@ -1,26 +1,30 @@
-import sharp from "sharp";
+import sharp from "sharp"
 
-const resizeImage = async (filename: string, width: number, height: number): Promise<string> => {
-    try {
-        const result = await sharp(`src/assets/full/${filename}.jpg`)
-            .resize({ width: width, height: height, fit: "cover" })
-            .toFile(`src/assets/thumb/${filename}.jpg`)
-            .then(() => {
-                return Promise.resolve(true)
-            })
-            .catch(() => {
-                return Promise.reject(false)
-            });
+const resizeImage = async (
+  filename: string,
+  width: number,
+  height: number
+): Promise<string> => {
+  try {
+    const result = await sharp(`src/assets/full/${filename}.jpg`)
+      .resize({ width: width, height: height, fit: "cover" })
+      .toFile(`src/assets/thumb/${filename}.jpg`)
+      .then(() => {
+        return Promise.resolve(true)
+      })
+      .catch(() => {
+        return Promise.reject(false)
+      })
 
-            if(result) {
-                return Promise.resolve("Success resize image")
-            }else {
-                return Promise.reject("Success resize image")
-            }
-    } catch (error: any) {
-        console.log(error);
-        throw(error)
+    if (result) {
+      return Promise.resolve("Success resize image")
+    } else {
+      return Promise.reject("Success resize image")
     }
-};
+  } catch (error: any) {
+    console.log(error)
+    throw error
+  }
+}
 
 export default resizeImage
