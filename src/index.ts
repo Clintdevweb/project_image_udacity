18f01fbfab3949d9744b21b73206c043240ea05e
hanhnv4 <hanhdevit@gmail.com>
2023-05-08 17:05:12 +0700

    [Fixbug] Remove file

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express()
 const port = 3000
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my project image")
+  res.send("Welcome to my project image") 
 })
 
 app.get("/api/images", async (req, res) => {
